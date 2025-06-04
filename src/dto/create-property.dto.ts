65274de9ea42b724c824205159cf51d054638835
Harrison devia <harrison.devia.mora@gmail.com>
2025-06-04 13:52:34 -0500

    create-property.dto

--- src/dto/create-property.dto.ts
+++ src/dto/create-property.dto.ts
@@ -1,8 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger';
 export class CreatePropertyDto {
+  @ApiProperty({ example: 'Casa en Playa' })
   title: string;
+
+  @ApiProperty({ example: 'Casa amplia con jardín y piscina.' })
   description: string;
+
+  @ApiProperty({ example: 350000 })
   price: number;
+
+  @ApiProperty({ example: 3 })
   bedrooms: number;
+
+  @ApiProperty({ example: 2 })
   bathrooms: number;
+
+  @ApiProperty({ example: 'Madrid, España' })
   location: string;
 }
