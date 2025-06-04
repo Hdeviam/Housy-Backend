b7364e325b69d8f97546aad1bad7a1b480cedc67
Harrison devia <harrison.devia.mora@gmail.com>
2025-06-04 00:04:02 -0500

    feat: entities.dto

---
+++ src/entities/property.entity.ts
@@ -0,0 +1,11 @@
+export class Property {
+  id?: number;
+  title: string;
+  description: string;
+  price: number;
+  bedrooms: number;
+  bathrooms: number;
+  location: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
