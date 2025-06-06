4db4b377905a92ad54931cbc1f231bca2118262c
Harrison devia <harrison.devia.mora@gmail.com>
2025-06-06 10:19:49 -0500

    src/app.modul

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { PropertiesModule } from './properties/properties.module';
+import { DatabaseModule } from './config/database.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, PropertiesModule, AuthModule],
+  imports: [AuthModule, UsersModule, PropertiesModule, AuthModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
