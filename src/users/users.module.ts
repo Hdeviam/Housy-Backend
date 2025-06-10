f99794e1820e3931da7d1ce59362deff3fba590a
Harrison devia <harrison.devia.mora@gmail.com>
2025-06-09 20:22:17 -0500

    src/users/users.module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
-import { UserRepository } from 'src/repository/user.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../entities/user.entity';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User]), JwtModule],
   controllers: [UsersController],
-  providers: [UsersService, UserRepository],
+  providers: [UsersService],
+  exports: [UsersService],
 })
 export class UsersModule {}
