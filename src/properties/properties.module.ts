38f6e13e87b62c9fe597b62f9ed4de7817c07cd4
Harrison devia <harrison.devia.mora@gmail.com>
2025-06-10 13:41:35 -0500

    src/properties/properties.module

--- src/properties/properties.module.ts
+++ src/properties/properties.module.ts
@@ -2,10 +2,17 @@ import { Module } from '@nestjs/common';
 import { PropertiesController } from './properties.controller';
 import { PropertiesService } from './PropertiesService';
 import { PropertyRepository } from 'src/repository/property.repository';
-import { AuthModule } from 'src/auth/auth.module'; // 👈 Importa AuthModule
+import { AuthModule } from 'src/auth/auth.module';
+import { UsersModule } from 'src/users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Property } from 'src/entities/property.entity';
 
 @Module({
-  imports: [AuthModule], // 👈 Asegura que JwtService esté disponible
+  imports: [
+    AuthModule,
+    UsersModule,
+    TypeOrmModule.forFeature([Property]), // 👈 Registra la entidad Property
+  ],
   controllers: [PropertiesController],
   providers: [PropertiesService, PropertyRepository],
 })
