3a721c1c17b9604a3cb82d9174ea8757168dc10f
Harrison devia <harrison.devia.mora@gmail.com>
2025-06-09 21:31:03 -0500

    src/dto/register.dto

--- src/dto/register.dto.ts
+++ src/dto/register.dto.ts
@@ -1,17 +1,14 @@
-import { IsEmail, IsString, IsStrongPassword } from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsString, MinLength } from 'class-validator';
 
 export class RegisterDto {
-  @ApiProperty({ example: 'user@example.com' })
   @IsEmail()
   email: string;
 
-  @ApiProperty({ example: 'John Doe' })
   @IsString()
+  @MinLength(3)
   name: string;
 
-  @ApiProperty({ example: 'SecurePass123!' })
   @IsString()
-  @IsStrongPassword()
+  @MinLength(6)
   password: string;
 }
