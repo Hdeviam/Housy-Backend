61a42e384adb1032958e23a6c4d75823968e049b
Harrison devia <harrison.devia.mora@gmail.com>
2025-06-09 19:30:08 -0500

    src/app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
 import { PropertiesModule } from './properties/properties.module';
 import { DatabaseModule } from './config/database.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [AuthModule, UsersModule, PropertiesModule, AuthModule, DatabaseModule],
