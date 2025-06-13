7a837e46916a1ac9eb6c235de0ca64885839d9b5
Harrison devia <harrison.devia.mora@gmail.com>
2025-06-13 13:01:45 -0500

    src/entities/enrichedPropertyParams.entity

---
+++ src/entities/enrichedPropertyParams.entity.ts
@@ -0,0 +1,45 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+} from 'typeorm';
+import { Property } from './property.entity';
+
+@Entity()
+export class EnrichedPropertyParams {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(() => Property, (property) => property.enrichedPropertyParams)
+  property: Property;
+
+  @Column({ type: 'uuid' })
+  propertyId: string;
+
+  @Column()
+  title: string;
+
+  @Column('text')
+  description: string;
+
+  @Column()
+  priceEstimate: number;
+
+  @Column('simple-array', { nullable: true }) // 👈 Guarda array como texto
+  recommendedPhotos: string[];
+
+  @Column()
+  qualityOfLifeScore: string;
+
+  @Column()
+  locationDetails: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
